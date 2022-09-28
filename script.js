cf6a1681ede1dc7ce4120575fb254eaf83f563b0
Yam Dangar <yamdangar@gmail.com>
2022-09-28 21:20:42 +1000

    bug fixed

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-const apiUrl = "https://randomuser.me/api/?results=20";
+const apiUrl = "https://randomuser.me/api/?";
+let userList = [];
 
 const displayElm = document.querySelector("#list");
-const fetchUsers = () => {
-  fetch(apiUrl)
+
+const fetchUsers = (params = "results=20") => {
+  fetch(apiUrl + params)
     .then((response) => response.json())
     .then((data) => {
       display(data.results);
+      userList = data.results;
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-fetchUsers();
-
 const display = (users) => {
   let str = "";
   console.log(users);
-
   users.map((item, i) => {
     str += `
-    <div class="card" style="width: 20rem">
-    <img src="${item.picture.large}" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">
-      ${item.name.title} ${item.name.first} ${item.name.last}</h5>
-      <p class="card-text">
-       <ul class = 'list-unstyled'> 
-       <li> <i class="fa-solid fa-mobile"></i> ${item.phone} </li>
-       <li> <i class="fa-solid fa-envelope"></i>${item.email} </li>
-       <li> <i class="fa-solid fa-calendar-days"></i> ${item.dob.date}</li>
-       <li class="d-flex> <i class="fa-solid fa-house"></i> ${item.location.street.number} ${item.location.street.name} ${item.location.postcode}${item.location.state}</li>
-       </ul>
-      </p>
-      
-    </div>
-  </div>
-    
-    `;
+            <div class="card m-2" style="width: 20rem">
+            <img src="${item.picture.large}" class="card-img-top" alt="..." />
+            <div class="card-body">
+            <h5 class="card-title">
+            ${item.name.title}  ${item.name.first}  ${item.name.last}
+            </h5>
+            <p class="card-text">
+            <ul class='list-unstyled'>
+            <li> <i class="fa-solid fa-mobile"></i> ${item.phone} </li>
+            <li > <i class="fa-solid fa-envelope"></i> ${item.email}</li>
+            <li> <i class="fa-solid fa-calendar-days"></i> ${item.dob.date}</li>
+            <li class="d-flex> <i class="fa-solid fa-house"></i> ${item.location.street.number} ${item.location.street.name}, ${item.location.postcode},  ${item.location.state}</li>
+            </ul>   
+            
+            </p>
+            
+            </div>
+            </div>
+`;
   });
+
   displayElm.innerHTML = str;
-  document.querySelector("#count").inner;
+
+  document.querySelector("#count").innerText = users.length;
 };
 
 fetchUsers();
 
-const handleOnChange = (e) => {
+const handleOnChage = (e) => {
   console.log(e.value);
-  const params = "results = 20&gender=" + e.value;
+  const params = "results=20&gender=" + e.value;
   fetchUsers(params);
 };
 
@@ -60,5 +63,6 @@ const handleOnSearch = (e) => {
 
     return userName.toLowerCase().includes(str.toLowerCase());
   });
+
   display(filteredUser);
 };
